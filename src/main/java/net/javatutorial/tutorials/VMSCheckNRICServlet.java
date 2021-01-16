7dcd74cc674a2663b6f807d1cb32b41fe5c095cf
Shangeri Sivalingam <anushangeri@gmail.com>
2021-01-15 20:27:07 -0500

    adding login credentials for multiple users

--- src/main/java/net/javatutorial/tutorials/VMSCheckNRICServlet.java
+++ src/main/java/net/javatutorial/tutorials/VMSCheckNRICServlet.java
@@ -2,15 +2,7 @@ package net.javatutorial.tutorials;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -20,6 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
+
+import com.google.gdata.client.spreadsheet.SpreadsheetService;
+import com.google.gdata.data.spreadsheet.CustomElementCollection;
+import com.google.gdata.data.spreadsheet.ListEntry;
+import com.google.gdata.data.spreadsheet.ListFeed;
 
 /**
  * Servlet implementation class TodHodSearchServlet
@@ -46,17 +43,46 @@ public class VMSCheckNRICServlet extends HttpServlet {
 				
 		boolean loginsuccessful = false;
 		
+		//CHECK K11CLICKS:DROPDOWN EXCEL VMS_ADMIN_CRED_12
+		SpreadsheetService service = new SpreadsheetService("K11CLICKS: DROPDOWN EXCEL");
 		if(!StringUtils.isEmpty(idNo)) {
+			loginsuccessful = true;
 			if(idNo.toUpperCase().equals("K11ADMIN")) {
-				loginsuccessful = true;
 				session.setAttribute("usertype", "K11ADMIN");
 			}
 			else{
-				loginsuccessful = true;
-				session.setAttribute("usertype", idNo);
+				try {
+		            String sheetUrl
+		                    = "https://spreadsheets.google.com/feeds/list/116L_MDacE0331uQDZLRQD4UKpKXfHgWKcMFeD0ne324/12/public/values";
+
+		            // Use this String as url
+		            URL url = new URL(sheetUrl);
+
+		            // Get Feed of Spreadsheet url
+		            ListFeed lf = service.getFeed(url, ListFeed.class);
+		            //Iterate over feed to get cell value
+		            for (ListEntry le : lf.getEntries()) {
+		                CustomElementCollection cec = le.getCustomElements();
+		                if (cec != null){
+		                    String nricfin = cec.getValue("nricfin").trim();
+		                    String usertype = cec.getValue("usertype").trim();
+		                	if(nricfin.equals(idNo)) {
+	                    		//if admin, don't set NRIC because admin can see everything
+	                    		session.setAttribute("usertype", usertype);
+	                    	}
+		                }
+		            }
+		            //if loop through google sheets and cannot find match means it is a public user not K11 STAFF
+		            if(session.getAttribute("usertype") == null) {
+		            	session.setAttribute("usertype", idNo);
+		            }
+		            
+		        }catch (Exception e) {
+					e.printStackTrace();
+				}
+				
 			}
 		}
-		
 		if(loginsuccessful) {
 			responseObj.add("Login successful.");
 			request.setAttribute("responseObj", responseObj);
@@ -66,7 +92,7 @@ public class VMSCheckNRICServlet extends HttpServlet {
 		else{
 			responseObj.add("NRIC/FIN is invalid. Please try again: " + idNo);
 			request.setAttribute("responseObj", responseObj);
-			rd = request.getRequestDispatcher("index.jsp");
+			rd = request.getRequestDispatcher("vmsCheckNRIC.jsp");
 			rd.forward(request, response);
 		}
         
