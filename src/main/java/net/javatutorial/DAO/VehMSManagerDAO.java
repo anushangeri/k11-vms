e10180031812aa4cae760a9a2ccc94cc37c31e0c
Shangeri Sivalingam <anushangeri@gmail.com>
2021-02-26 22:37:37 -0500

    fix nextval method getting increment for vms instead of vehms

--- src/main/java/net/javatutorial/DAO/VehMSManagerDAO.java
+++ src/main/java/net/javatutorial/DAO/VehMSManagerDAO.java
@@ -26,11 +26,7 @@ public class VehMSManagerDAO {
 	        		+  "(VEHICLE_ID, NAME, COMPANY_NAME, ID_TYPE, ID_NO, MOBILE_NO, PRIME_MOVER_NO, CONTAINER_NO, SEAL_NO, CONTAINER_SIZE,"
 	        		+ " LOADED_FLAG, COVID_DECLARE_FLAG, LORRY_CHET_NO, DELIVERY_NOTICE_NO,"
 	        		+ " VISIT_PURPOSE, TEMPERATURE, TIME_IN_DT)" + 
-	        		"  VALUES ('" +v.getVehicleId()+ "','" +v.getName()+ "','" +v.getCompanyName()+ "','" +v.getIdType()+ "','" 
-	        		+v.getIdNo()+ "','" +v.getMobileNo()+ "','" +v.getPrimeMoverNo()+ "','" +v.getContainerNo()+ "','" 
-	        		+v.getSealNo()+ "','" +v.getContainerSize()+ "','" +v.getLoadedNoLoaded()+ "','" +v.getCovidDeclare()+ "','" +v.getLorryChetNumber()+ "','" 
-	        		+v.getDeliveryNoticeNumber()+ "','" +v.getVisitPurpose()+ "','" 
-	        		+v.getTemperature()+ "','"  +v.getTimeInDt()+ "')");
+	        		"  VALUES ('1','t','','NRIC','409R','',' ','','','','','','','','','','')");
 	        rs = stmt.executeQuery("SELECT NAME FROM VEHMS WHERE VEHICLE_ID ='" + v.getVehicleId() +"';");
 	        while (rs.next()) {
 	        	message = "Read from DB: " + rs.getTimestamp("tick");
