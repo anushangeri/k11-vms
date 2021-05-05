50335952108cae3c3008420c9e4feffc6c9385eb
Shangeri Sivalingam <anushangeri@gmail.com>
2021-05-04 20:34:45 -0400

    add site to client account so staff can only see records from a certain site in charge

--- src/main/java/net/javatutorial/tutorials/ProcessPasswordServlet.java
+++ src/main/java/net/javatutorial/tutorials/ProcessPasswordServlet.java
@@ -67,7 +67,7 @@ public class ProcessPasswordServlet extends HttpServlet {
 			rd.forward(request, response);
 		}
 		else {
-			request.setAttribute("responseObj", "Wrong NRIC and Password entered. Please try again.");
+			request.setAttribute("responseObj",c.toString());
 			RequestDispatcher rd = request.getRequestDispatcher("clientLogin.jsp");
 			rd.forward(request, response);
 		}
