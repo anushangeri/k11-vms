fc3dc096701e96b7ab6a1f3dfc60242b2378c4a6
Shangeri Sivalingam <anushangeri@gmail.com>
2021-07-08 00:19:38 -0400

    trying to get schedulerMain to work

--- src/main/java/com/heroku/devcenter/SchedulerMain.java
+++ src/main/java/com/heroku/devcenter/SchedulerMain.java
@@ -1,15 +1,11 @@
 package com.heroku.devcenter;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SchedulerMain {
 
-    final static Logger logger = LoggerFactory.getLogger(SchedulerMain.class);
     
     public static void main(String[] args) throws Exception {
-    	logger.info("Message Sent: shangeri over heerrreee");
     	System.out.println("Message Sent: shangeri over heerrreee");
     }
 
