19e4447c4d7277d764850ead755768194f37273d
Shangeri Sivalingam <anushangeri@gmail.com>
2021-07-09 15:21:51 -0400

    trying to get batch to run

--- src/main/java/net/javatutorial/tutorials/Main.java
+++ src/main/java/net/javatutorial/tutorials/Main.java
@@ -7,21 +7,21 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-    
-    public static void main(String[] args) throws Exception {
-        
-        Connection connection = getConnection();
-        
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-        while (rs.next()) {
-            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
-        }
-    }
-    
+//    
+//    public static void main(String[] args) throws Exception {
+//        
+//        Connection connection = getConnection();
+//        
+//        Statement stmt = connection.createStatement();
+//        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+//        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+//        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+//        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+//        while (rs.next()) {
+//            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+//        }
+//    }
+//    
     public static Connection getConnection() throws URISyntaxException, SQLException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
