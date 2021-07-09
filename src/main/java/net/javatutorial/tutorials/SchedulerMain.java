fe1de7ef54c80d51fce5e6c05cab1cd60f00c318
Shangeri Sivalingam <anushangeri@gmail.com>
2021-07-09 14:56:47 -0400

    trying to get batch to run

---
+++ src/main/java/net/javatutorial/tutorials/SchedulerMain.java
@@ -0,0 +1,13 @@
+package com.heroku.devcenter;
+
+import net.javatutorial.DAO.VMSArchiveManagerDAO;
+
+public class SchedulerMain {
+
+    
+    public static void main(String[] args) throws Exception {
+    	String message = VMSArchiveManagerDAO.moveVisitor();
+    	System.out.println("Visitor Records Moved: " + message);
+    }
+
+}
