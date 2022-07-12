fa8f265aab1ca8c16786036ede1817233089b5be
anushangeri <anushangeri@gmail.com>
2022-07-12 17:17:25 -0400

    test OTP

--- src/main/java/net/javatutorial/tutorials/SendOTPSMSServlet.java
+++ src/main/java/net/javatutorial/tutorials/SendOTPSMSServlet.java
@@ -5,15 +5,9 @@ import java.sql.Timestamp;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
-import java.util.Properties;
 import java.util.Random;
+import java.util.Scanner;
 
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -36,7 +30,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-
 
 /**
  * Servlet implementation class SendOTPSMSServlet
@@ -97,41 +90,11 @@ public class SendOTPSMSServlet extends HttpServlet {
 		System.out.println("Servlet " + this.getServletName() + " has stopped");
 	}
 	public String sendSms(String mobileNo) {
-		String to = "6476093381@sms.k11.com.sg";// change accordingly
-		final String user = "Shangeri1994@k11.com.sg";// change accordingly
-		final String password = "EbSDkwr+Hvc7!U57";// change accordingly
-		Transport myTransport = null;
-		
-		Properties properties = System.getProperties();
-		properties.setProperty("mail.smtp.host", "smtp.k11.com.sg");
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.port", "25");
-
-		Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(user, password);
-			}
-		});
-		
-		try {
-			// Construct data
-			//String apiKey = "apikey=" + "NzY0ZDQ5NjczODU3NTI3NjRmNmI2MzY3NTY2MzZiNzI=";
-			Random rand = new Random();
-			int otpGenerated = rand.nextInt(999999);
-			
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(user));
-			InternetAddress[] address = {new InternetAddress(to)};
-			message.setRecipients(Message.RecipientType.TO, address);
-			message.setSubject("OTP");
-			message.setText(otpGenerated + "");
-
-			 myTransport = session.getTransport("smtp");
-			 myTransport.connect("smtp.k11.com.sg", user, password);
-			 message.saveChanges();
-			  myTransport.sendMessage(message, message.getAllRecipients());
-			  myTransport.close();
+//		try {
+//			// Construct data
+//			String apiKey = "apikey=" + "NzY0ZDQ5NjczODU3NTI3NjRmNmI2MzY3NTY2MzZiNzI=";
+//			Random rand = new Random();
+//			int otpGenerated = rand.nextInt(999999);
 //			String message = "&message=" + "Hi visitor, your OTP is " + otpGenerated;
 //			String sender = "&sender=" + "K11 VMS";
 //			String numbers = "&numbers=" + mobileNo;
@@ -151,10 +114,55 @@ public class SendOTPSMSServlet extends HttpServlet {
 //			}
 //			rd.close();
 //			System.out.println(stringBuffer.toString());
-			return otpGenerated + "";
-		} catch (Exception e) {
-			System.out.println("Error SMS "+e);
-			return "Error "+e;
+//			return otpGenerated + "";
+//		} catch (Exception e) {
+//			System.out.println("Error SMS "+e);
+//			return "Error "+e;
+//		}
+		
+		try {
+			Scanner sc = new Scanner(System.in);
+
+			
+			String host = "localhost";
+			int port = 9500;
+			String username = "admin";
+			String password = "abc123";
+			
+			/**
+			 * Connect to Ozeki NG SMS Gateway and logging in.
+			 */
+			
+			MyOzSmsClient osc = new MyOzSmsClient(host, port);
+			osc.login(username, password);		
+			System.out.println("SMS message:");
+			
+			/**
+			 * If logged in send "Text message" to number "+00112233"
+			 */
+			
+			if(osc.isLoggedIn()) {
+					
+				osc.sendMessage("+16476093381", "Test Message");				
+				
+			}
+			
+			sc.nextLine();
+			osc.logout();
+			
+			/**
+			 * Receiving message:
+			 * 
+			 * If you want to receive messages you can use doOnMessageReceived in MyOzSmsClient.java
+			 * That's an event, which runs automatically when a message is received. 
+			 */
+			
+		} catch (IOException e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
 		}
 	}
 }
