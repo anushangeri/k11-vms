b0acee8af794a7207cc09e4a0c3ff51785ac9dc8
anushangeri <anushangeri@gmail.com>
2022-07-12 23:03:59 -0400

    test please wokr

---
+++ src/main/java/net/javatutorial/tutorials/SendOTP.java
@@ -0,0 +1,49 @@
+package net.javatutorial.tutorials;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SendOTP {
+
+    
+    public static void main(String[] args) throws Exception {
+    	URL url = new URL(System.getenv("BLOWERIO_URL") + "messages");
+		
+		Map<String,Object> params = new LinkedHashMap<>();
+        params.put("to", "+16476093381");
+        params.put("message", "This is a test from Blower.io");
+        
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+		
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+        for (int c; (c = in.read()) >= 0;)
+            System.out.print((char)c);
+        
+
+		
+    }
+
+}
--- src/main/java/net/javatutorial/tutorials/SendOTPSMSServlet.java
+++ src/main/java/net/javatutorial/tutorials/SendOTPSMSServlet.java
@@ -40,11 +40,15 @@ import java.net.URLEncoder;
 public class SendOTPSMSServlet extends HttpServlet {
 	private static final long serialVersionUID = -4751096228274971485L;
 	
-	@SuppressWarnings("deprecation")
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
+		try {
+			SendOTP.main(null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		int nextVal = 1; //this value does not matter since we are not adding visitor yet
 		
@@ -77,29 +81,9 @@ public class SendOTPSMSServlet extends HttpServlet {
 		ArrayList<Site> siteDropdown = SiteManagerDAO.retrieveAll();
 		ArrayList<Dropdown> visitPurposes = DropdownListManagerDAO.retrieveByDropdownKey("VISIT_PURPOSE");
  
-		URL url = new URL(System.getenv("BLOWERIO_URL") + "messages");
 		
-		HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-		httpConn.setRequestMethod("POST");
-
-		httpConn.setRequestProperty("Accept", "application/json");
-		httpConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-		httpConn.setDoOutput(true);
-		OutputStreamWriter writer = new OutputStreamWriter(httpConn.getOutputStream());
-		writer.write("to=+14155550000&message=This is a test from Blower.io");
-		writer.flush();
-		writer.close();
-		httpConn.getOutputStream().close();
-
-		InputStream responseStream = httpConn.getResponseCode() / 100 == 2
-				? httpConn.getInputStream()
-				: httpConn.getErrorStream();
-		Scanner s = new Scanner(responseStream).useDelimiter("\\A");
-		String responsed = s.hasNext() ? s.next() : "";
-		System.out.println(responsed);
-        
-
+         
+         
          
 		request.setAttribute("visitorLatRec", v);
 		request.setAttribute("siteDropdown", siteDropdown);
@@ -116,36 +100,5 @@ public class SendOTPSMSServlet extends HttpServlet {
 	public void destroy() {
 		System.out.println("Servlet " + this.getServletName() + " has stopped");
 	}
-	public String sendSms(String mobileNo) {
-		try {
-			// Construct data
-			String apiKey = "apikey=" + "NzY0ZDQ5NjczODU3NTI3NjRmNmI2MzY3NTY2MzZiNzI=";
-			Random rand = new Random();
-			int otpGenerated = rand.nextInt(999999);
-			String message = "&message=" + "Hi visitor, your OTP is " + otpGenerated;
-			String sender = "&sender=" + "K11 VMS";
-			String numbers = "&numbers=" + "+16476093381";
-			
-			// Send data
-			HttpURLConnection conn = (HttpURLConnection) new URL("https://api.txtlocal.com/send/?").openConnection();
-			String data = apiKey + numbers + message + sender;
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-			conn.getOutputStream().write(data.getBytes("UTF-8"));
-			final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			final StringBuffer stringBuffer = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				stringBuffer.append(line);
-			}
-			rd.close();
-			System.out.println(stringBuffer.toString());
-			return otpGenerated + "";
-		} catch (Exception e) {
-			System.out.println("Error SMS "+e);
-			return "Error "+e;
-		}
-		
-	}
+
 }
