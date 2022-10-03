c2bc19f4b5811df0d901136d96283345611f2e86
anushangeri <anushangeri@gmail.com>
2022-10-03 10:43:12 -0400

    error

--- src/main/java/net/javatutorial/tutorials/ArchiveRecordsServlet.java
+++ src/main/java/net/javatutorial/tutorials/ArchiveRecordsServlet.java
@@ -73,7 +73,7 @@ public class ArchiveRecordsServlet extends HttpServlet {
 		// check to see if the given date really is the last day of this month
 		System.out.println( "check to see if the given date really is the last day of this month");
 		System.out.println( cal.get(Calendar.DAY_OF_MONTH) == lastDay);
-		if(cal.get(Calendar.DAY_OF_MONTH) != lastDay) {
+		if(cal.get(Calendar.DAY_OF_MONTH) == lastDay) {
 			System.out.println( "it is the last day of the month, so batch job will do archive db clean up and email results");
 			
 			try 
