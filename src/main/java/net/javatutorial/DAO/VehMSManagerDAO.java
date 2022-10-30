9dbbf1a4edf96e4fc9376bedaeea58b649ddf186
anushangeri <anushangeri@gmail.com>
2022-10-30 18:51:17 -0400

    fixing records count

--- src/main/java/net/javatutorial/DAO/VehMSManagerDAO.java
+++ src/main/java/net/javatutorial/DAO/VehMSManagerDAO.java
@@ -395,7 +395,7 @@ public class VehMSManagerDAO {
             		+ "VISIT_PURPOSE, TEMPERATURE, SEAL_NO, CONTAINER_SIZE, REMARKS, "
             		+ "WAREHOUSE_LEVEL, SITE, WAREHOUSE_APPROVER, TIME_IN_DT, TIME_OUT_DT,"
             		+ "CREATED_BY, CREATED_BY_DT, LAST_MODIFIED_BY, LAST_MODIFIED_BY_DT \r\n"
-            		+ "FROM VEHMS WHERE DATE(TIME_IN_DT) = DATE(NOW()) ORDER BY TIME_IN_DT DESC; ";
+            		+ "FROM VEHMS WHERE DATE(TIME_IN_DT) = '2022-10-31' ORDER BY TIME_IN_DT DESC; ";
             pstmt = connection.prepareStatement(sql);
 
             rs = pstmt.executeQuery();
