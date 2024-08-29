171ab8e4aed29b9ced7513195ae0407fff8a0da6
anushangeri <anushangeri@gmail.com>
2024-08-29 21:43:36 +0800

    dynamic emails

--- src/main/java/net/javatutorial/tutorials/ArchiveRecordsServlet.java
+++ src/main/java/net/javatutorial/tutorials/ArchiveRecordsServlet.java
@@ -112,7 +112,7 @@ public class ArchiveRecordsServlet extends HttpServlet {
 		// check to see if the given date really is the last day of this month
 		System.out.println( "check to see if the given date really is the last day of this month");
 		System.out.println( cal.get(Calendar.DAY_OF_MONTH) == lastDay);
-		if(cal.get(Calendar.DAY_OF_MONTH) != lastDay) {
+		if(cal.get(Calendar.DAY_OF_MONTH) == lastDay) {
 			System.out.println( "it is the last day of the month, so batch job will do archive db clean up and email results");
 			
 			final String password = new String(System.getenv("EMAIL_PASSWORD"));
@@ -411,7 +411,7 @@ public class ArchiveRecordsServlet extends HttpServlet {
 				workbook.close();
 				// out.close();
 
-				String to = "anushangeri@gmail.com";// change accordingly
+				String to = "k11.sivalingam@gmail.com";// change accordingly
 				final String user = "shangeri.sivalingam@k11.com.sg";// change accordingly
 				//final String password = "Sh@ngeri94";// change accordingly
 				
